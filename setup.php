445f49e3a67f5915e6806663ba9fad347701f8a0
brian9952 <fbriann@gmail.com>
2020-12-28 07:04:26 +0700

    setup.php patch #1

--- setup.php
+++ setup.php
@@ -9,31 +9,31 @@ Setting up database
 <?php
 	require_once 'function.php';
 
-	createTable('User',
+  createTable('User',
     'UserId INT(32) NOT NULL AUTO_INCREMENT PRIMARY KEY,
     Email VARCHAR(32),
-  	Password VARCHAR(32),
-  	LastActivity DATE,
-  	IsOnline BOOL,
+    Password VARCHAR(32),
+    LastActivity DATE,
+    IsOnline BOOL,
     IsSupplier BOOL
     IsAdmin BOOL');
 
-	createTable('UserInfo',
+  createTable('UserInfo',
     'UserId INT(32) NOT NULL,
-  	Name VARCHAR(100),
-  	Description TEXT,
-  	Company VARCHAR(48),
-  	Location VARCHAR(48),
-  	Website VARCHAR(64),
-  	PhoneNumber VARCHAR(16),
-  	FOREIGN KEY (UserId) REFERENCES User (UserId) ON DELETE CASCADE ON UPDATE CASCADE');
+    Name VARCHAR(100),
+    Description TEXT,
+    Company VARCHAR(48),
+    Location VARCHAR(48),
+    Website VARCHAR(64),
+    PhoneNumber VARCHAR(16),
+    FOREIGN KEY (UserId) REFERENCES User (UserId) ON DELETE CASCADE ON UPDATE CASCADE');
 
-	createTable('UserPhoto',
+  createTable('UserPhoto',
     'CREATE TABLE UserPhoto(
-  	PhotoId INT(32) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  	UserId INT(32) NOT NULL,
-  	PhotoName VARCHAR(48),
-  	FOREIGN KEY (UserId) REFERENCES User (UserId) ON DELETE CASCADE ON UPDATE CASCADE');
+    PhotoId INT(32) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    UserId INT(32) NOT NULL,
+    PhotoName VARCHAR(48),
+    FOREIGN KEY (UserId) REFERENCES User (UserId) ON DELETE CASCADE ON UPDATE CASCADE');
 
   createTable('ChatSystem',
     'ChatSystemId INT(32) NOT NULL AUTO_INCREMENT PRIMARY KEY,
